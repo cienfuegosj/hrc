142eba5ec43a93926d64f177e7e992af57729772
Human Rights Commission <hrc@tango-server.berea.edu>
2016-11-08 11:45:50 -0500

    Report Data Extraction & Database Connection initiated

--- __init__.py
+++ __init__.py
@@ -27,10 +27,36 @@ def survey():
 def register_page():
      try:
     	c, conn=connection()
-    	return ("Okay")
      except Exception as e:
         return (str(e))
+        
+@app.route('/report/<report_selection>')
+def BuildReport(report_selection):
 
+  '''
+    Builds a report by querying the database and asking for the selected information.
+    We also build a list to then render the report_modal.html template.
+    param: selection is of type string so we can use it to query to the database.
+  '''
+  
+  #Connect to the database
+  try:
+    c, conn = connection()
+  except Exception as e:
+    return str(e)
+    
+  #Query the database and extract data in a form of a container datatype
+  
+  
+  #render the template 'report_modal.html' with the list parameter.
+  
+  
+  #Use Jinja in report_modal.html to display informationls
+  
+@app.route('/reportinformation')
+def ReportInfo(data):
+  return render_template ("report_modal.html", data)
+  
 
 if __name__ == "__main__":
     app.run(debug = True, host="0.0.0.0", port=5006)
