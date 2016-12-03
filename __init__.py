5a0908942f15b2026f9c9137912037c69ab28e33
Human Rights Commission <hrc@tango-server.berea.edu>
2016-12-03 18:15:59 -0500

    Survey updated

--- __init__.py
+++ __init__.py
@@ -3,6 +3,8 @@ from db_connect import connection
 from DBConnection import DBConnection
 import logging
 import datetime
+import pdfkit
+import copy
 
 app = Flask(__name__)
 
@@ -52,9 +54,8 @@ def result():
      return render_template("result.html", msg = msg)
    
    return "Ok"
-       
-
       
+
 
 @app.route('/reports')
 def reportspage():
@@ -63,10 +64,75 @@ def reportspage():
 
 
 
-@app.route('/survey')
+@app.route('/survey/')
 def survey():
     '''this function displays the survey page'''
     return render_template ("survey.html")
+
+
+@app.route('/result2/', methods=['POST', 'GET'])
+def result2():
+   '''this function handles the post method and sends the values to the database'''
+
+   if request.method == 'POST':       # Checks if the method is a post method
+     #check = request.form
+     try:
+     	titles = request.form['title_data']       # gets the title value
+     	dates = request.form['date_data']    # gets the value of the date
+     	locations= request.form['location_data']       # gets the location value
+     	type2 =request.form['types_data']    # gets the value of the event type
+     	govts=request.form['govt_data']
+     	#residents=request.form['residency_data']   #gets the value of the residency
+     	residents = request.form.get('residency_data', None)
+     	if residents is None:
+        	residents="None"
+     	races=request.form.get('race_data', None)    # gets the race value
+     	if races is None:
+        	races="None"
+     	sexes=request.form.get('sex_data', None)
+     	if sexes is None:
+        	sexes="None"
+     	ages=request.form.get('age_data', None)
+     	if ages is None:
+        	ages="None" 
+       	########## the lines below gets the values of the questions from numbers 1-17
+     	one=request.form['q1']
+     	two=request.form['q2']
+     	a3=request.form['q3']
+     	a4=request.form['q4']
+     	a5=request.form['q5']
+     	a6=request.form['q6']
+     	a7=request.form['q7']
+     	a8=request.form['q8']
+     	a9=request.form['q9']
+     	a10=request.form.get('q10_data', None)
+     	if a10 is None:
+        	a10="None"
+     	a11=request.form.get('q11_data', None)
+     	if a11 is None:
+        	a11="None"
+     	a12=request.form['q12']
+     	a13=request.form['q13']
+     	a14=request.form['q14']
+     	a15=request.form['q15']
+     	a16=request.form['q16']
+     	a17=request.form.get('q17_data', None)
+     	if a17 is None:
+        	a17="None"
+     	c, conn = connection()        # connects to the database
+     	c.execute("INSERT INTO survey(location, eventype, title, date, residency, race, sex, age, govt, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17)VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",(locations, type2, titles, dates, residents, races, sexes, ages, govts, one, two, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17) ) #gets the query
+     	conn.commit()     # inserts the query into the database
+     	msg = "Data successfully added"  # gets a successful message
+     	c.close()     # closes the connection
+     	conn.close()    # closes the final connnection
+
+     except:
+      	msg = "Insert operation failed. Ensure you fill out all the required information in the survey next time."       # gets message if the connection fails
+
+     return render_template("result2.html", msg=msg)
+     #return render_template("result2.html", check=check)
+   return "Ok"
+
 
 
 @app.route('/register')
@@ -99,25 +165,84 @@ def BuildReport(report_selection):
     
   # Query the database and extract data in a form of a container datatype
   
-  rawdata = conn.query("SELECT * FROM event")
+  raweventdata = conn.query("SELECT * FROM event")
   
   attendance_dic_data = {'Speaker': 0, 'Panel':0, 'Training':0, 'Forum':0, 'Festival':0, 'Other':0}
   finance_dic_data = {'Speaker': 0, 'Panel':0, 'Training':0, 'Forum':0, 'Festival':0, 'Other':0}
-  other_dic_data = {'Speaker': 0, 'Panel':0, 'Training':0, 'Forum':0, 'Festival':0, 'Other':0}
+  
+  #QDO = Question Dictionary Options
+  qdo = {'N/A': 0, 'Strongly Disagree': 0, 'Disagree': 0, 'No opinion': 0,
+  						'Agree': 0, 'Strongly Agree': 0
+  						}
+  
+  
+  questions_dic_data = {'q1': copy.deepcopy(qdo), 'q2': copy.deepcopy(qdo), 'q3': copy.deepcopy(qdo),
+  					 	'q4': copy.deepcopy(qdo), 'q5': copy.deepcopy(qdo), 'q6': copy.deepcopy(qdo),
+  					 	'q7': copy.deepcopy(qdo), 'q8': copy.deepcopy(qdo), 'q9': copy.deepcopy(qdo),
+  					 	'q12': copy.deepcopy(qdo), 'q13': copy.deepcopy(qdo), 'q14': copy.deepcopy(qdo),
+  					 	'q15': copy.deepcopy(qdo)
+  						}
+
+  rawsurveydata = conn.query("SELECT * FROM survey")
   
   if report_selection == "attendance":
-  	for item in rawdata:
+  	for item in raweventdata:
   		attendance_dic_data[item['type']] += item['attendance']
   elif report_selection == "finance":
-  	for item in rawdata:
+  	for item in raweventdata:
   		finance_dic_data[item['type']] += item['finance']
   else:
-  	for item in rawdata:
-  		other_dic_data[item['type']] += item['other']
-  	
-  del conn  
+  	for i in range(1,10):
+  		for item in rawsurveydata:
+  			if item['q' + str(i)] == 'N/A':
+				questions_dic_data['q' + str(i)]['N/A'] += 1
+			elif item['q' + str(i)] == 'Strongly Disagree':
+				questions_dic_data['q' + str(i)]['Strongly Disagree'] += 1
+			elif item['q' + str(i)] == 'Disagree':
+				questions_dic_data['q' + str(i)]['Disagree'] += 1
+			elif item['q' + str(i)] == 'No opinion':
+				questions_dic_data['q' + str(i)]['No opinion'] += 1
+			elif item['q' + str(i)] == 'Agree':
+				questions_dic_data['q' + str(i)]['Agree'] += 1
+			elif item['q' + str(i)] == 'Strongly Agree':
+				questions_dic_data['q' + str(i)]['Strongly Agree'] += 1
+	for i in range(12, 16):
+  		for item in rawsurveydata:
+  			if item['q' + str(i)] == 'N/A':
+				questions_dic_data['q' + str(i)]['N/A'] += 1
+			elif item['q' + str(i)] == 'Strongly Disagree':
+				questions_dic_data['q' + str(i)]['Strongly Disagree'] += 1
+			elif item['q' + str(i)] == 'Disagree':
+				questions_dic_data['q' + str(i)]['Disagree'] += 1
+			elif item['q' + str(i)] == 'No opinion':
+				questions_dic_data['q' + str(i)]['No opinion'] += 1
+			elif item['q' + str(i)] == 'Agree':
+				questions_dic_data['q' + str(i)]['Agree'] += 1
+			elif item['q' + str(i)] == 'Strongly Agree':
+				questions_dic_data['q' + str(i)]['Strongly Agree'] += 1
+
+	
+  del conn
   
-  return render_template("currentreport.html", selection = report_selection, adata = attendance_dic_data, fdata = finance_dic_data, odata = other_dic_data)
+  return render_template("currentreport.html", selection = report_selection,
+  						 adata = attendance_dic_data, fdata = finance_dic_data,
+  						 qdata = questions_dic_data)
+  
+@app.route('/reportpdf/<selection>', methods = ['GET','POST'])
+def ReportPDF(selection):
+	'''
+	Creates the pdf version of the html in order to retrieve the bar chart.
+	selection is a string variable that holds either attendance, finance, or other.
+	'''
+	
+	if selection == 'Attendance':
+		pdfkit.from_url('http://tango.berea.edu:5006/report/attendance', 'Attendance.pdf')
+	elif selection == 'Finance':
+		pdfkit.from_url('http://tango.berea.edu:5006/report/finance', 'Finance.pdf')
+	elif selection == 'Survey Statistics':
+		pdfkit.from_url('http://tango.berea.edu:5006/report/survey', 'SurveyStatistics.pdf')
+		
+	return redirect("tango.berea.edu:5006/home")
 
 if __name__ == "__main__":
     app.run(debug = True, host="0.0.0.0", port=5006)
